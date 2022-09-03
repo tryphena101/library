d0984aebf63399ea109f1e09793933e01bb21475
tryphena101 <tryph101@hotmail.com>
2022-09-04 02:34:58 +0800

    updates to modal box

--- index.js
+++ index.js
@@ -14,19 +14,43 @@ const modal = document.getElementById("addBookModal")
 //Get button that opens the modal
 const btn = document.getElementById("addBook")
 
-const span = document.getElementsByClassName("close")
+const span = document.querySelector(".close")
+
+const overlay = document.querySelector(".overlay")
 
 //When the user clicks the button, open the modal Form
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// document.querySelector('.modal').addEventListener('click', e => {
+//   document.body.classList.add('show-modal')
+// })
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+//
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+//
+//
 // window.onclick = function(event) {
 //   if (event.target == modal) {
 //     modal.style.display = "none";
 //   }
 // }
+
+
+const openAddBookModal = () => {
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+const closeBookModal = () => {
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+
+
+btn.onclick = openAddBookModal;
+span.onclick = closeBookModal;
+overlay.onclick = closeBookModal;
